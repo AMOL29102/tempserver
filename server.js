94f97ca74cf27ccc04e2630ff383050ad43abee3
Amol <amolpatilamp2910@gmail.com>
2024-08-07 19:10:41 +0530

    all3

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const port = 9000;
 app.use("/",(req,res)=>{
     res.json({message: "Hello server!!" });
 });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+
+
 app.listen(9000, () => {
     console.log(`Starting the server at : ${port}`);
 });
